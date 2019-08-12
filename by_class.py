5141b2f2ec37b6bd2325ae1d9a270ef19b37ae7b
huyh66 <huyh66@chinaunicom.cn>
2019-08-12 20:54:40 +0800

    提交一版

--- by_class.py
+++ by_class.py
@@ -1,17 +1,20 @@
 import json
+import pickle
 from time import sleep
 from typing import List
 
 from pyquery import PyQuery
 from requests import request
+from tqdm import tqdm
 
 
 class Product:
     def __init__(self):
         self.title: str = ''
         self.url: str = ''
-        self.comment_number: int = ''
+        self.comment_count: int = ''
         self.mall: str = ''
+        self.collection_count: int = ''
 
 
 class Spider:
@@ -23,7 +26,7 @@ class Spider:
         self.products: List[Product] = []
 
     def get_products(self):
-        for page_index in range(1, 30):
+        for page_index in tqdm(range(3, 5)):
             html = request('GET', self.base_url + str(page_index), headers=self.request_headers).text
             if html is not None and html != '':
                 # 初始化PyQuery
@@ -38,13 +41,18 @@ class Spider:
                 items: List[PyQuery] = doc(".z-group-data").items()
                 for (index, item) in enumerate(items):
                     if index % 2 == 1:
-                        self.products[int(index / 2)].comment_number = int(item.attr('title').replace('评论数 ', ''))
-
-            sleep(1)
+                        self.products[int(index / 2)].comment_count = int(item.attr('title').replace('评论数 ', ''))
+            sleep(2)
 
 
 if __name__ == '__main__':
     spider = Spider()
     spider.get_products()
     for product_cls in spider.products:
-        print(product_cls)
+        print(product_cls.__dict__)
+    spider.products.sort(key=lambda x: x.comment_count, reverse=True)
+    print("-----------------------------------------------------------------------------------------------------")
+    for product_cls in spider.products:
+        print(product_cls.__dict__)
+    with open('by_class.list', 'wb') as f:
+        pickle.dump(spider.products, f)
---
+++ view.py
@@ -0,0 +1,4 @@
+import pickle
+
+with open('by_class.list', 'rb') as f:
+    products = pickle.load(f)
