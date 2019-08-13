c0c0bc794a5e4dced22a4da5ec1ec24e0c13b8db
huyh66 <huyh66@chinaunicom.cn>
2019-08-13 14:14:14 +0800

    真正完成按类别爬取

--- by_class.py
+++ by_class.py
@@ -21,18 +21,20 @@ class Product:
 class Spider:
 
     def __init__(self):
-        self.base_url: str = 'https://www.smzdm.com/fenlei/yidongyingpan/h1c1s0f0t0p'
+        self.base_url: str = 'https://www.smzdm.com/fenlei/bijibendiannao/h1c1s0f0t0p'
         self.request_headers: dict = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36"}
         self.products: List[Product] = []
 
     def get_products(self):
-        for page_index in tqdm(range(1, 20)):
+        for page_index in tqdm(range(1, 5)):
             html = request('GET', self.base_url + str(page_index), headers=self.request_headers).text
             if html is not None and html != '':
                 # 初始化PyQuery
                 doc: PyQuery = PyQuery(html)
                 contents: List[PyQuery] = doc(".z-feed-content > .z-highlight > a").items()
+                # 用于后期遍历追加index下标防止每次都只写第一页数组
+                accumulate_index = self.products.__len__()
                 for content in contents:
                     product = Product()
                     product.title = eval(content.attr('onclick')[15:-1])['pagetitle']
@@ -45,9 +47,10 @@ class Spider:
                 for (index, foot) in enumerate(foots):
                     if isinstance(foot, PyQuery):
                         if index % 2 == 0:
-                            self.products[int(index / 2)].comment_count = int(foot.text())
+                            # index的运算多加一个括号是为了强调先加然后在除的顺序
+                            self.products[int(accumulate_index + (index / 2))].comment_count = int(foot.text())
                         else:
-                            self.products[int(index / 2)].collection_count = int(foot.text())
+                            self.products[int(accumulate_index + (index / 2))].collection_count = int(foot.text())
             sleep(2)
 
 
@@ -57,5 +60,5 @@ if __name__ == '__main__':
     spider.products.sort(key=lambda x: x.comment_count, reverse=True)
     for product_cls in spider.products:
         print(product_cls.__dict__)
-    with open('yidongyingpan.list', 'wb') as f:
+    with open('bijibendiannao.list', 'wb') as f:
         pickle.dump(spider.products, f)
