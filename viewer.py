78433a2cd1610898279946791fb4f42ec6985a8f
huyh66 <huyh66@chinaunicom.cn>
2019-08-13 19:09:27 +0800

    添加过滤方法

--- viewer.py
+++ viewer.py
@@ -7,13 +7,17 @@ from model import Product
 class Viewer:
     def __init__(self, file_name):
         self.file_name: str = file_name
+        with open(self.file_name, 'rb') as f:
+            self.products: List[Product] = pickle.load(f)
 
     def view_products(self):
-        with open(self.file_name, 'rb') as f:
-            products: List[Product] = pickle.load(f)
-
-        for product in products:
+        for product in self.products:
             print(product.__dict__)
 
+    def filter_by_price(self, price):
+        self.products = list(filter(lambda x: x.price < price, self.products))
 
-Viewer('yaseshi.list').view_products()
+
+v = Viewer('bijibendiannao.list')
+v.filter_by_price(4500)
+v.view_products()
