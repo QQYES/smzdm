ca9e17a4111a079a7fe0d2e5752592ee3409aec2
huyh66 <huyh66@chinaunicom.cn>
2019-08-14 09:34:13 +0800

    修改异常处理

--- by_class.py
+++ by_class.py
@@ -1,5 +1,6 @@
 import pickle
 import re
+import sys
 from time import sleep
 from typing import List
 
@@ -13,7 +14,7 @@ from model import Product, ClassProduct
 class Spider:
 
     def __init__(self):
-        self.base_url: str = 'https://www.smzdm.com/fenlei/paobuxie/h1c1s0f0t0p'
+        self.base_url: str = 'https://www.smzdm.com/fenlei/bijibenneicun/h1c1s0f0t0p'
         self.request_headers: dict = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36"}
         self.products: List[Product] = []
@@ -28,20 +29,20 @@ class Spider:
                 # 用于后期遍历追加index下标防止每次都只写第一页数组
                 for content in contents:
                     product = ClassProduct()
-                    up = content('div.z-highlight > a').items().__next__()
-                    product.title = eval(up.attr('onclick')[15:-1])['pagetitle']
-                    product.url = up.attr('href')
-                    product.mall = eval(up.attr('onclick')[15:-1])['商城']
                     try:
+                        up = content('div.z-highlight > a').items().__next__()
+                        product.title = eval(up.attr('onclick')[15:-1])['pagetitle']
+                        product.url = up.attr('href')
+                        product.mall = eval(up.attr('onclick')[15:-1])['商城']
                         product.price = float(re.findall(r"\d+\.?\d*", up.text())[0])
-                    except IndexError:
+                        product.comment_count = int(
+                            content('.icon-comment-o-thin + span').items().__next__().text())
+                        product.collection_count = int(
+                            content('.icon-star-o-thin + span').items().__next__().text())
+                        self.products.append(product)
+                    except Exception as e:
+                        print("异常错误信息：".format(e))
                         print("价格获取错误，错误内容:{}".format(product.__dict__))
-                    product.comment_count = int(
-                        content('.icon-comment-o-thin + span').items().__next__().text())
-                    product.collection_count = int(
-                        content('.icon-star-o-thin + span').items().__next__().text())
-                    self.products.append(product)
-
             sleep(2)
 
 
@@ -51,5 +52,5 @@ if __name__ == '__main__':
     spider.products.sort(key=lambda x: x.comment_count, reverse=True)
     for product_cls in spider.products:
         print(product_cls.__dict__)
-    with open('paobuxie', 'wb') as f:
+    with open('bijibenneicun', 'wb') as f:
         pickle.dump(spider.products, f)
