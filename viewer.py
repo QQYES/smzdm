f134e0306b75c032766457478567e5b62171a531
huyh66 <huyh66@chinaunicom.cn>
2019-12-18 19:23:03 +0800

    提交一版

--- viewer.py
+++ viewer.py
@@ -24,7 +24,7 @@ class Viewer:
         self.products = list(filter(lambda x: not x.title.__contains__(keywords), self.products))
 
 
-v = Viewer('data/白酒_160')
+v = Viewer('data/电饭煲_60')
 # v.filter_by_price(4500)
 # v.filter_by_mall('京东')
 # v.filter_by_keywords('芝华仕')
