a94c1b81f5a32ab855729445e77dc804afd7626f
huyh66 <huyh66@chinaunicom.cn>
2020-10-26 16:36:08 +0800

    提交一版

--- by_class.py
+++ by_class.py
@@ -50,7 +50,7 @@ class Spider:
 
 
 if __name__ == '__main__':
-    spider = Spider('https://www.smzdm.com/fenlei/qichezhengche/h1c1s0f0t0p', 30)
+    spider = Spider('https://www.smzdm.com/fenlei/xiyiji/h1c1s0f0t0p', 165)
     spider.get_products()
     spider.products.sort(key=lambda x: x.comment_count, reverse=True)
     for product_cls in spider.products:
